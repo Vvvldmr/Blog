37ed5646e55563f593110386ff40b8b1f5db7319
Vladimir Firsov <vladimirfirsov987@gmail.com>
2025-06-29 15:09:20 +0700

    Добавил отображение одного отдельного поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import get_post_list
+from .views import get_post_list, post_detail
+
 
 urlpatterns = [
-    path('posts/', get_post_list)
+    path('posts/', get_post_list, name='post_list'),
+    path('posts/<int:post_id>', post_detail, name='post_detail')
 ]
