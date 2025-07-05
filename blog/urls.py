e09fd4de18a45a9d766d598ecd4190d7b2bff37f
Vladimir Firsov <vladimirfirsov987@gmail.com>
2025-07-05 16:41:36 +0700

    Добавил функционал для создания нового поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import get_post_list, post_detail
+from .views import get_post_list, post_detail, create_post
 
 
 urlpatterns = [
     path('posts/', get_post_list, name='post_list'),
-    path('posts/<int:post_id>', post_detail, name='post_detail')
+    path('posts/<int:post_id>', post_detail, name='post_detail'),
+    path('posts/add', create_post, name='create_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
@@ -12,3 +12,16 @@ def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def create_post(request):
+    if request.method == "GET":
+        return render(request, 'blog/post_add.html')
+    
+    if request.method == "POST":
+        user_title = request.POST.get('title')
+        user_text = request.POST.get('text')
+
+        post = Post.objects.create(title = user_title, content = user_text)
+
+        return redirect('post_detail', post_id = post.id)
