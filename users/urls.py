9ff89bd725d28d600b7de1aa74f4dd952b1f734c
Vladimir Firsov <vladimirfirsov987@gmail.com>
2025-07-27 15:59:59 +0700

    Добавил функционал входа и выхода из аккаунта

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import register_view
+from . import views
+
+# app_name = 'users'
 
 urlpatterns = [
-    path('register', register_view, name='register_view')
+    path('register', views.register_view, name='register_view'),
+    path('login', views.login_view, name='login_view'),
+    path('logout', views.logout_view, name='logout_view')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == "POST":
@@ -8,10 +9,31 @@ def register_view(request):
         if form.is_valid():
             form.save()
 
-            return redirect('post_list')
+            return redirect('login_view')
         else:
             return render(request, 'users/register.html', context={'form': form})
     
     form = UserCreationForm()
 
     return render(request, 'users/register.html', context={'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect('post_list')
+        else:
+            return render(request, 'users/login.html', context={'form': form})
+
+    form = AuthenticationForm()
+
+    return render(request, 'users/login.html', context={'form': form})
+
+def logout_view(request):
+    logout(request)
+
+    return redirect('post_list')
