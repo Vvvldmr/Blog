6348c4100e054f76d61ee760d20e4c6456e70d81
Vladimir Firsov <vladimirfirsov987@gmail.com>
2025-08-24 12:38:27 +0700

    Добавил слаг

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from unidecode import unidecode
 
 User = get_user_model()
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, unique=True, verbose_name = 'Заголовок')
+    slug = models.SlugField(max_length=200, unique=True, editable=False, verbose_name = 'Слаг')
     content = models.TextField()
     image = models.ImageField(upload_to="post_images/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,3 +15,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(unidecode(self.title))
+
+        super().save(*args, **kwargs)
